0b9c24967f7073e6f0d1a309c32c449318c75ced
Bu Kinoshita <bukinoshita@gmail.com>
2019-12-16 14:58:26 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,18 @@
 import test from 'ava'
 import { isOn, toggle } from '.'
 
-if (process.env.CI) {
+import test from 'ava'
+import { isOn, toggle } from '.'
+
+if (!process.env.CI) {
   // Travis doesn't have hotplug
   test('travis', t => {
     t.pass()
   })
-} else {
+} else {  
   test('main', async t => {
     t.true(await isOn())
     await toggle()
     t.false(await isOn())
-  })
-  
-  test.after(async () => {
-    await toggle()
   })
 }
